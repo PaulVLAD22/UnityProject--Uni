8fc3e305d22f39d2f9533bf910beeb8b40af162f
paulvlad22 <paulvlad34@gmail.com>
2021-10-19 21:57:59 +0300

    .

---
+++ UnityProject--Uni/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float OX, OY, OZ;
+    private Vector3 direction;
+    public int speed = 10;
+    private Rigidbody rb;
+    private static float breakStrength = 0.9f;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OY = 0;
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OX = Input.GetAxis("Horizontal");
+        OZ = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.Space))
+        {
+            slowDown();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        direction = new Vector3(OX, OY, OZ);
+        rb.AddForce(direction * speed);
+        //Debug.Log(OX+" "+OZ );
+    }
+
+    public void slowDown()
+    {
+        this.rb.velocity *= breakStrength;
+    }
+
+    
+}
