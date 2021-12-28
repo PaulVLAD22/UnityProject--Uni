33e2200a0a1c24ee68ad47cf2b9f1a25a7509e44
Vlad Munteanu <paulvlad34@gmail.com>
2021-12-28 16:03:01 +0200

    feat(bots): bullets destroyed 2 sec

---
+++ map project/Assets/ColissionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColissionHandler : MonoBehaviour
+{
+    int numberOfColides;
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if(collision.gameObject.tag =="Player"){
+            //damge code
+            Debug.Log("Take Damage");
+        }
+        // if(numberOfColides==2)
+        //     Destroy(gameObject);
+    }
+}
